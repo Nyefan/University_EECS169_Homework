5eec748bcfd950090fa5130e7dc4567eac19070d
Davis St. Aubin <nyefan@aol.com>
2013-04-09 10:29:11 -0500

    v 0.11

--- HW#3/FightClub/src/fightclub/FightClub.java
+++ HW#3/FightClub/src/fightclub/FightClub.java
@@ -10,11 +10,11 @@ import java.util.Scanner;
  *  Homework Number: 03						|
  *  Description: TODO - WRITE DESCRIPTION OF THE PROGRAM HERE	|
  *  File Created: Apr 6, 2013					|
- *  Last Changed: Apr 6, 2013					|
+ *  Last Changed: Apr 8, 2013					|
  ---------------------------------------------------------------*/
 
 public class FightClub {
-  private static int turns;
+  private static int turn;
   private static Fighter fighter1;
   private static Fighter fighter2;
   private static boolean shouldLoop;
@@ -25,8 +25,7 @@ public class FightClub {
     initialize(fighter1);
     initialize(fighter2);
     do {
-      play();
-      shouldLoop = checkShouldLoop();
+      shouldLoop = play();
     } while(shouldLoop);
 
   }
@@ -38,26 +37,62 @@ public class FightClub {
     fighters++;
   }
 
-  public static void play() {
-
-    fighter1.takeTurn(getTurnType());
-    fighter2.takeTurn(getTurnType());
-
-    turns++;
-
+  public static boolean play() { //TODO: make first hit work properly;
+    System.out.println(firstHit().getName() + " will attack first.");
+    
+    for(turn = 0; turn < 10; turn++) {
+      System.out.printf("Round %d:", turn);
+      
+      if(fighter1.takeTurn(getTurnType(), fighter2)) {
+        turn = 0;
+        if(checkShouldLoop()) {
+          initialize(fighter2);
+          return true;
+        }
+        else return false;
+      }
+      
+      else if(fighter2.takeTurn(getTurnType(), fighter1)) {
+        turn = 0;
+        if(checkShouldLoop()) {
+          initialize(fighter1);
+          return true;
+        }
+        else return false;
+      }
+    }
+    
+    //TODO: handle case where turns run out;
+    
   }
 
-  public static int getTurnType() {
-
+  public static char getTurnType() {
+    System.out.println("Please select an action...");
+    if(turn == 0) {
+      System.out.println("'H' or 'h' for Haste"
+              + "\n'S' or 's' for Spirit Guard"
+              + "\n'A' or 'a' to Attack");
+    }
+    return keyboard01.nextLine().charAt(0);
   }
 
+  public static Fighter firstHit() {
+    double f1a = fighter1.rollAttack();
+    double f2a = fighter2.rollAttack();
+    if(f1a > f2a) return fighter1;
+    else if(f2a > f1a) return fighter2;
+    else return firstHit();
+  }
+  
   private static boolean checkShouldLoop() { // Note[01];
     System.out.print("\nDo you want to try another ball?"
 	    + "\nPlease enter 1 for Yes, 0 for No: ");
     //Grab the first character of the user's response for if/else checking
     char chkChar = keyboard01.next().charAt(0);
 
-    if (chkChar == '1' || chkChar == 'y' || chkChar == 'Y') {return true;}
+    if (chkChar == '1' || chkChar == 'y' || chkChar == 'Y') {
+      
+      return true;}
     else if (chkChar == '0' || chkChar == 'n' || chkChar == 'N') {
       System.out.println("\nGoodbye!");
       return false;
--- HW#3/FightClub/src/fightclub/Fighter.java
+++ HW#3/FightClub/src/fightclub/Fighter.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  *  Homework Number: 03						|
  *  Description: TODO - WRITE DESCRIPTION OF THE PROGRAM HERE	|
  *  File Created: Apr 6, 2013					|
- *  Last Changed: Apr 6, 2013					|
+ *  Last Changed: Apr 8, 2013					|
  ---------------------------------------------------------------*/
 
 public class Fighter {
@@ -84,48 +84,55 @@ public class Fighter {
     shouldLoop = false;
   }
 
-  public boolean takeTurn(char turnType, Fighter opponent) { //TODO: System.out.print statements describing action
-    if(turnType == 'a' || turnType == 'A') {
-      return magAS();
+  public boolean takeTurn(char turnType, Fighter opponent) {
+    if(turnType == 'h' || turnType == 'H') {
+      System.out.println(name + " has cast Haste, increasing his attack speed by one!");
+      return magAS(); //Always returns false;
     }
-    else if(turnType == 'd' || turnType == 'D') {
-      return magDL();
+    else if(turnType == 's' || turnType == 'S') {
+      System.out.println(name + " has cast Spirit Guard, increasing his defense level by one!");
+      return magDL(); // Always returns false;
     }
     else {
       return attack(opponent);
     }
   }
   private boolean attack(Fighter opponent) {
-    double att = rollAttack();
-    double oppdef = opponent.rollDefense();
-
-    if(att < oppdef) {return false;}
-    else {
-      opponent.hitFor( rollAttack() ); //TODO: round up rollAttack's return value;
-      if(opponent.getHP() <= 0) {return true;}
+    if(rollAttack() > opponent.rollDefense()) {
+      System.out.println(name + " hits " + opponent.name + " for "
+              + opponent.hitFor( (int) rollAttack() + 1 ) // rounds up rollAttack's return value
+              + "points!  " + opponent.name + "has " + opponent.hitpoints + " HP remaining.");
+      if(opponent.hitpoints <= 0) {return true;}
       else {return false;}
+    }
+    else {
+      System.out.println(name + " misses " + opponent.name + ".");
+      return false;
     }
   }
 
   private boolean magAS() {
     attackSpeed++; return false;
   }
-
   private boolean magDL() {
     defenseLevel++; return false;
   }
 
-  private double rollAttack() {
+  public double rollAttack() {
     return random.nextDouble() * attackSpeed;
   }
   public double rollDefense() {
     return random.nextDouble() * defenseLevel;
   }
-  public void hitFor(int points) {
+  public int hitFor(int points) {
     hitpoints -= points;
+    return points;
   }
   public int getHP() {
     return hitpoints;
+  }
+  public String getName() {
+    return name;
   }
 
 }
